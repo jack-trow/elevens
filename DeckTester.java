bb7ab26acb8aa51a62893db4d2ee7a498b21c05d
Jack Trowbridge <101287756+jack-trow@users.noreply.github.com>
2023-12-06 09:35:13 -0500

    Create DeckTester.java

---
+++ DeckTester.java
@@ -0,0 +1,16 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		Card one = new Card("Five", "Hearts",5);
+		Card two = new Card("Six", "Hearts",6);
+		Card three = new Card("Five", "Spades",5);
+	}
+}
